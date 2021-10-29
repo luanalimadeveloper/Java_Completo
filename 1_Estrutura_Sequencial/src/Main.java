862c292cc263378b96efd21d724117f04512cf4f
luanalimadeveloper <luanalima.developer@gmail.com>
2021-10-28 23:33:01 -0300

    first project

---
+++ 1_Estrutura_Sequencial/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		////////////////////////////////////
+		/// Saida de dados em Java
+		
+		int  y = 32;
+		double x = 10.35896;
+		String nome = "Luana";
+		int idade = 28;
+		double renda = 4000.0; 
+		
+		System.out.print("Olá mundo!");
+		System.out.println("Tudo bom ?");
+		
+		System.out.println(y);
+		System.out.println(x);
+		
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f%n", x);
+		
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.4f%n", x);
+
+		System.out.println("Resultado = " + x +  " metros");
+		System.out.printf("Resultado =  %.2f metros%n", x);
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais %n", nome, idade, renda);
+
+	}
+
+}
