b0e1f27453ed23800061e1cd220b4491f0c6ab98
luanalimadeveloper <luanalima.developer@gmail.com>
2021-11-01 20:47:07 -0300

    Atribuicao Acumulativa

--- 2_Estrutura_Condicional/src/Main.java
+++ 2_Estrutura_Condicional/src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		////////////////////////////////////////////////
+		// Estrutura Condicional (if-else)
+		/*
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -19,6 +24,24 @@ public class Main {
 			System.out.println("Boa Noite");
 		}
 		
+		sc.close();
+		
+		*/
+		////////////////////////////////////////////////
+		/// Atribuição Acumulativa 
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int minutos = sc.nextInt();
+
+		double conta = 50.0;
+		if (minutos > 100) {
+			conta += (minutos - 100) * 2.0;
+		}
+
+		System.out.printf("Vamor da conta = R$ %.2f %n", conta);
+
 		sc.close();
 	}
 
