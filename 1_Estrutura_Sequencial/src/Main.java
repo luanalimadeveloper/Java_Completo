ab9888e55888c7360c278fff628ea22c17f94db1
luanalimadeveloper <luanalima.developer@gmail.com>
2021-11-01 11:33:24 -0300

    Saida de dados em Java - Exercicio

--- 1_Estrutura_Sequencial/src/Main.java
+++ 1_Estrutura_Sequencial/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		
 		////////////////////////////////////
 		/// Saida de dados em Java
+		/*
 		
 		int  y = 32;
 		double x = 10.35896;
@@ -28,6 +29,31 @@ public class Main {
 		System.out.println("Resultado = " + x +  " metros");
 		System.out.printf("Resultado =  %.2f metros%n", x);
 		System.out.printf("%s tem %d anos e ganha R$ %.2f reais %n", nome, idade, renda);
+
+
+		*/
+		////////////////////////////////////
+		/// Saida de dados em Java - Exercicio 
+		
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		System.out.printf("%s, which price is $ %.2f %n", product1, price1);
+		System.out.printf("%s, which price is $ %.2f %n", product2, price2);
+		System.out.printf("Record: %d years old, code %d and gender %c %n", age, code, gender);
+		System.out.printf("Measure with eight decimal places: %.8f %n", measure);
+		System.out.printf("Rouded (three decimalplaces): %.3f %n", measure);
+		
+		Locale.setDefault(Locale.US);
+		System.out.printf("US decimal point: %.3f", measure);
 
 	}
 
