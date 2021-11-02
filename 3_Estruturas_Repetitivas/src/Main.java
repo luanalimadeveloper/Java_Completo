64078a3c011bfe1f5bc886973ccc6d73b7b713ea
luanalimadeveloper <luanalima.developer@gmail.com>
2021-11-01 23:27:04 -0300

    Estrutura Repetitiva (do - while)

--- 3_Estruturas_Repetitivas/src/Main.java
+++ 3_Estruturas_Repetitivas/src/Main.java
@@ -44,7 +44,8 @@ public class Main {
 	
 	*/
 	//////////////////////////////////////////
-	/// Estrutura repetitiva (for)
+	/// Estrutura Repetitiva (for)
+	/*
 	
 	Scanner sc = new Scanner(System.in);
 	
@@ -58,7 +59,28 @@ public class Main {
 	
 	System.out.println(soma);
 	sc.close();
-
+	
+	*/
+		
+	//////////////////////////////////////////
+	/// Estrutura Repetitiva (do - while)
+	
+	Locale.setDefault(Locale.US);
+	Scanner sc = new Scanner(System.in);
+	
+	char resp;
+	
+	do {
+	System.out.print("Temperatura em Celsius: ");
+	double C = sc.nextDouble();
+	double F = 9.0 * C / 5.0 + 32.0; 
+	
+	System.out.printf("Equivalente em Fahrenheit: %.2f%n", F);
+	System.out.print("Deseja Repetir (s/n) ?");
+	resp = sc.next().charAt(0);
+	
+	} while (resp != 'n');
+	
+	sc.close();
 	}
-
 }
