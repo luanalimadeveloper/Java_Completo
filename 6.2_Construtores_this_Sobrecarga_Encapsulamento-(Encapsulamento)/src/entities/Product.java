2561cae5e5028d54f48e5ffab9f51ad18555ad68
luanalimadeveloper <luanalima.developer@gmail.com>
2021-11-05 22:34:28 -0300

    Modificacao no toString

--- 6.2_Construtores_this_Sobrecarga_Encapsulamento-(Encapsulamento)/src/entities/Product.java
+++ 6.2_Construtores_this_Sobrecarga_Encapsulamento-(Encapsulamento)/src/entities/Product.java
@@ -58,7 +58,11 @@ public class Product {
 	}
 
 	public String toString() {
-		return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, Total: $ "
-				+ String.format("%.2f", totalValueInStock());
+		return name 
+			   + ", $ " 
+			   + String.format("%.2f", price) 
+			   + ", " + quantity 
+			   + " units, Total: $ "
+			   + String.format("%.2f", totalValueInStock());
 	}
 }
