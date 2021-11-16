b627baecbdd80cbc753c7264971bc338a25d1062
luanalimadeveloper <luanalima.developer@gmail.com>
2021-11-15 23:49:29 -0300

    entities do projeto

--- 27_Enumeracao_Composicao-(Composicao)/src/application/Program.java
+++ 27_Enumeracao_Composicao-(Composicao)/src/application/Program.java
@@ -1,10 +1,18 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.print("Enter department's name");
+		
+		sc.close();
 	}
 
 }
