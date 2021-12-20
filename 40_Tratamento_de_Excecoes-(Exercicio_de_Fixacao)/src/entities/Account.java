a1a592fbde81d503e8ca7d62d22ef9c05147e49b
luanalimadeveloper <luanalima.developer@gmail.com>
2021-12-19 23:17:17 -0300

    Exercicio de fixacao - Excecoes

---
+++ 40_Tratamento_de_Excecoes-(Exercicio_de_Fixacao)/src/entities/Account.java
@@ -0,0 +1,59 @@
+package entities;
+
+import model.exception.DomainException;
+
+public class Account {
+	
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
+	
+	public Account() {
+		super();
+	}
+
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+	
+	public void deposit(Double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(Double amount) throws DomainException {
+		balance -= amount;
+		if (balance <= 0) {
+			throw new DomainException("Not enough balance !");
+		}
+		
+		if(amount > withdrawLimit) {
+			throw new DomainException("The amount exceeds withdraw limit!");
+		}
+
+	}
+	
+}
