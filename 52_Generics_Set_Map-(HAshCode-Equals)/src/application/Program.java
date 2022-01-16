3e9128faf74809eb3273253e3da691c41f28f0c3
luanalimadeveloper <luanalima.developer@gmail.com>
2022-01-15 23:53:32 -0300

    exercicios generics set e map

---
+++ 52_Generics_Set_Map-(HAshCode-Equals)/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import entities.Client;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Client c1 = new Client("Maria", "maria@gmail.com");
+		Client c2 = new Client("Maria", "maria@gmail.com");
+		
+		String s1 = new String("Test");
+		String s2 = new String("Test");
+		
+		String s3 = "Test";
+		String s4 = "Test";
+		
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));
+		System.out.println(c1 == c2); // Em posiçoes diferente de memoria
+		System.out.println(s1 == s2); // Compilador realiza tratamento especial
+		System.out.println(s3 == s4); // Sem tratamento especial
+		
+		
+		
+	}
+
+}
