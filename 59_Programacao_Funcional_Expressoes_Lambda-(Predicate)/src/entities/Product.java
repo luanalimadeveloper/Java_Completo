b3e3b45a5af807b9d71aa98843e62f0a03ac3aa8
luanalimadeveloper <luanalima.developer@gmail.com>
2022-01-18 17:39:40 -0300

    Expressão lambda inline

--- 59_Programacao_Funcional_Expressoes_Lambda-(Predicate)/src/entities/Product.java
+++ 59_Programacao_Funcional_Expressoes_Lambda-(Predicate)/src/entities/Product.java
@@ -4,7 +4,7 @@ public class Product {
 
 	private String name;
 	private Double price;
-
+	
 	public Product(String name, Double price) {
 		this.name = name;
 		this.price = price;
@@ -26,6 +26,14 @@ public class Product {
 		this.price = price;
 	}
 
+	public static boolean staticProductPredicate(Product p) {
+		return p.getPrice() >= 100.0;
+	}
+	
+	public boolean nonStaticProductPredicate() {
+		return price >= 100.0;
+	}
+	
 	@Override
 	public String toString() {
 		return name + ", " + String.format("%.2f", price);
